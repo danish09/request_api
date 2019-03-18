f413afd94b4ba23bf5cbcbb4fdb2010333089169
Danish Siddiqui <danishsiddiqui09@gmail.com>
2019-03-18 14:57:40 +0000

    imported requests library

--- nginx_api.py
+++ nginx_api.py
@@ -1,5 +1,13 @@
 ips = {}
 
+import requests
+
+# Make a get request to get the latest position of the international space station from the opennotify api.
+response = requests.get("http://api.open-notify.org/iss-now.json")
+# Print the status code of the response.
+print(response.status_code)
+
+
 fh = open("/var/log/nginx/access.log", "r").readlines()
 for line in fh:
     ip = line.split(" ")[0]
