f63eeab92571691cd7bb27fa7708d22bf0473065
Danish Siddiqui <danishsiddiqui09@gmail.com>
2019-03-19 16:05:09 +0000

    reading api which spit out json data

--- nginx_api.py
+++ nginx_api.py
@@ -1,16 +1,12 @@
-ips = {}
-
 import requests
 
-# Make a get request to get the latest position of the international space station from the opennotify api.
-response = requests.get("http://api.open-notify.org/iss-now.json")
-# Print the status code of the response.
-print(response.status_code)
 
-
-fh = open("/var/log/nginx/access.log", "r").readlines()
-for line in fh:
-    ip = line.split(" ")[0]
-    if 6 < len(ip) <=15:
-        ips[ip] = ips.get(ip, 0) + 1
-print ips
+# Set up the parameters we want to pass to the API.
+# This is the latitude and longitude of New York City.
+parameters = {"lat": 28.7041, "lon": 77.1025}
+# Make a get request with the parameters.
+response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
+# Print the content of the response (the data the server returned)
+print(response.content)
+# This gets the same data as the command aboveresponse = requests.get("http://api.open-notify.org/iss-pass.json?lat=40.71&lon=-74")
+#print(response.content)
