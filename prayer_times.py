685b2c84007b469cbfa711b4f1ec63ada92ecb65
Danish Siddiqui <danishsiddiqui09@gmail.com>
2019-03-21 12:50:48 +0000

    getting specific key value

--- prayer_times.py
+++ prayer_times.py
@@ -1,12 +1,15 @@
 import requests
+import json
+#from pprint import pprint
 
 #setting the parameters
 
-parameters = {"address": '14 Plympton Place, London, United Kingdom', "method": 12, "month": 04, "year": 2019}
+parameters = {"address": '14 Plympton Place, London, NW8 8AD, United Kingdom', "method": 12, "month": '04', "year": 2019}
 
 #calling the API with params
 
 response = requests.get("http://api.aladhan.com/v1/calendarByAddress", params=parameters)
+#jsonresponse = json.dumps(requests.get("http://api.aladhan.com/v1/calendarByAddress", params=parameters).json())
 
 # Headers is a dictionary
 #print(response.headers)
@@ -21,6 +24,10 @@ data = response.json()
 #print(type(data))
 
 #printing the json converted data
-print(data)
+#print(data)
 
+print(data['data'][0])
+
+
+#pprint(data)
 #print(response.content)
