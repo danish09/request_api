07f281ab5ddb30122525dcf52d6bba29078fd800
Danish Siddiqui <danishsiddiqui09@gmail.com>
2019-03-25 16:43:50 +0000

    formatting statements

--- prayer_times_v2.py
+++ prayer_times_v2.py
@@ -8,27 +8,15 @@ from playsound import playsound
 
 tday=datetime.today().strftime('%Y-%m-%d')
 
-yr=datetime.today().strftime('%Y')
-
-mon=datetime.today().strftime('%m')
-
 right_now=datetime.today().strftime('%I-%M-%p')
 
-response = requests.get("https://www.londonprayertimes.com/api/times/?format=json&key=0239f686-4423-408e-9a0c-7968a403d197&year=yr&month=mon")
+response = requests.get("https://www.londonprayertimes.com/api/times/?format=json&key=0239f686-4423-408e-9a0c-7968a403d197&year=&month=")
 
 data=response.json()
 
-times_access = data['times']
-
-times_access = read_content['times']
-
-dates_access = times_access[tday]
-
-for key,value in dates_access.items():
-    if value == tday:
-#        print (key, "=>", value)
-        if value == right_now:
-            print('it is asr time')
+for key,value in data.items():
+    if value >= '03:30' and value < '06:00':
+            print('It is asr time')
     
     
     #playsound('/home/danish/Downloads/adan.mp3')
