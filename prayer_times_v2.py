ed194445599fb3859b5d79c8bc68a9f27bd071a3
Danish Siddiqui <danishsiddiqui09@gmail.com>
2019-03-25 16:31:22 +0000

    pulled the json api without optional parameters

--- prayer_times_v2.py
+++ prayer_times_v2.py
@@ -1,18 +1,34 @@
 import json
 
+import requests
+
 from datetime import datetime
 
 from playsound import playsound
 
 tday=datetime.today().strftime('%Y-%m-%d')
 
-with open('/home/danish/repo/hub/request_api/iccuk_prayertimes') as json_file:
-    read_content = json.load(json_file)
+yr=datetime.today().strftime('%Y')
+
+mon=datetime.today().strftime('%m')
+
+right_now=datetime.today().strftime('%I-%M-%p')
+
+response = requests.get("https://www.londonprayertimes.com/api/times/?format=json&key=0239f686-4423-408e-9a0c-7968a403d197&year=yr&month=mon")
+
+data=response.json()
+
+times_access = data['times']
 
 times_access = read_content['times']
 
 dates_access = times_access[tday]
 
 for key,value in dates_access.items():
-    print (key, "=>", value)
+    if value == tday:
+#        print (key, "=>", value)
+        if value == right_now:
+            print('it is asr time')
+    
+    
     #playsound('/home/danish/Downloads/adan.mp3')
